7d0b6352f21fbe03173cba2a7ef9458bf3a4cd5d
ptrshk <rostislavtrushko@gmail.com>
2024-01-21 15:19:42 +0300

    feat: add 5th task solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+
+fs.readdir(
+  '05-merge-styles/styles',
+  {withFileTypes: true},
+  (err, dirents) => {
+    if (err) {
+      console.log('Folder "styles" doesn\'t exist')
+    } else {
+      let dataArr = [];
+      (async () => {
+        for (let dirent of dirents) {
+          if (dirent.type = 1 && dirent.name.split('.')[1] === 'css') {
+            await fs.promises.readFile(
+              `05-merge-styles/styles/${dirent.name}`,
+              )
+            .then(data => {
+                dataArr.push(data.toString());
+              }
+            )
+          }
+        }
+      }
+      )()
+      .then(() => {
+          if (!dataArr.length) {
+            console.log('there is no css files');
+          } else {
+            fs.writeFile(
+              '05-merge-styles/project-dist/bundle.css',
+              dataArr.join('\n'),
+              (err) => {if (err) {console.log(err)} else {console.log('css files succesfully merged')}}
+            )
+          }
+        }
+      )
+    }
+  }
+)
+
+
+
+
+
+
+      // dirents.forEach(
+      //    (dirent) => {
+      //     if (dirent.type = 1 && dirent.name.split('.')[1] === 'css') {
+      //       fs.promises.readFile(
+      //         `05-merge-styles/styles/${dirent.name}`,
+      //         // (err, data) => {
+      //         //   if (err) {
+      //         //     console.log(err);
+      //         //   } else {
+      //         //     dataArr.push(data.toString());
+      //         //     console.log(dataArr.length);
+      //         //   }
+      //         // }
+      //       )
+      //       .then(data => {
+      //           dataArr.push(data.toString());
+      //           console.log(dataArr.length);
+      //         }
+      //       )
+      //     }
+      //   }
+      // )
+
+      
